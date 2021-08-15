ca6a5cd53f1a75972bac7f682f5e69a4afff1209
DonCanjas <80267631+DonCanjas@users.noreply.github.com>
2021-08-15 19:15:12 +0100

    Update ivtc.py

--- vs/ivtc.py
+++ vs/ivtc.py
@@ -5,26 +5,26 @@ from vsutil import depth, get_depth
 core = vs.core
 
 
-def IVTC(clip: vs.VideoNode, TFF: Optional[bool] = None) -> vs.VideoNode:
+def IVTC(clip: vs.VideoNode) -> vs.VideoNode:
     """"
-        Experimental script for inverse telecining and deinterlacing made by LightArrowsEXE <https://github.com/LightArrowsEXE/dotfiles/blob/master/mpv/.config/mpv/vs/ivtc.vpy>
+        Experimental script for inverse telecining and deinterlacing.
+        Forked from <https://github.com/LightArrowsEXE/dotfiles/blob/master/mpv/.config/mpv/vs/ivtc.vpy>
 
         Requires VapourSynth <http://www.vapoursynth.com/doc/about.html>
 
         Additional dependencies:
             * vsutil <https://pypi.org/project/vsutil/>
+            * TIVTC <https://github.com/dubhater/vapoursynth-tivtc>		
+				TIVTC's documentation <http://avisynth.nl/index.php/TIVTC/TFM>
 
         :param clip:         Input clip
-        :param TFF:          Top-Field-First
 
         :return:             IVTC'd clip
     """
-    if TFF is None:
-        TFF = True
 
     down = depth(clip, 8)
-    vfm = core.vivtc.VFM(down, TFF)
-    return depth(vfm, get_depth(clip))
+    tfm = core.tivtc.TFM(down)
+    return depth(tfm, get_depth(clip))
 
 
-IVTC(video_in, TFF=True).set_output()
+IVTC(video_in).set_output()
