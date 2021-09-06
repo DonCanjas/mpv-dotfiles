53f0abf5ee05841235c20531b5668f56e1401261
DonCanjas <80267631+DonCanjas@users.noreply.github.com>
2021-09-06 23:08:29 +0100

    Update camcorder_color_denoise.py

--- vs/camcorder_color_denoise.py
+++ vs/camcorder_color_denoise.py
@@ -3,8 +3,6 @@ import ccd
 
 core = vs.core
 
-rgb = core.resize.Spline36(video_in, format=vs.RGBS)
-
-derainbow = core.ccd.CCD(rgb, 25)
+derainbow = ccd.ccd(video_in, 10, "709")
 
 derainbow.set_output()
