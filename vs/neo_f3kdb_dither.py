598ef94ca5d2f86033efd55e34f6157a5df141eb
DonCanjas <80267631+DonCanjas@users.noreply.github.com>
2021-10-02 22:24:16 +0100

    Update neo_f3kdb_dither.py

--- vs/neo_f3kdb_dither.py
+++ vs/neo_f3kdb_dither.py
@@ -1,4 +1,4 @@
 import vapoursynth as vs
-core = vs.get_core()
+core = vs.core
 
 core.neo_f3kdb.Deband(video_in, range=15, y=40, cb=0, cr=0, grainy=64, grainc=0, dynamic_grain=True, sample_mode=2).set_output()
