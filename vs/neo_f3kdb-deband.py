c8eb9a1e07c3eb30b9b92993878acac9e9caac21
DonCanjas <80267631+DonCanjas@users.noreply.github.com>
2022-01-27 12:54:38 +0000

    Update neo_f3kdb-deband.py

--- vs/neo_f3kdb-deband.py
+++ vs/neo_f3kdb-deband.py
@@ -1,4 +1,4 @@
 import vapoursynth as vs
 core = vs.core
 
-core.neo_f3kdb.Deband(video_in, range=15, y=64, cb=16, cr=16, grainy=64, grainc=0, dynamic_grain=True, sample_mode=2).set_output()
+core.neo_f3kdb.Deband(video_in, range=30, y=64, cb=32, cr=32, grainy=64, grainc=0, dynamic_grain=True, sample_mode=2).set_output()
