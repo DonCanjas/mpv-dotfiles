7547b1307f18bc141a163a0e727ae9fe0d2cbb54
DonCanjas <80267631+DonCanjas@users.noreply.github.com>
2022-07-07 22:23:48 +0100

    Delete vs directory

--- vs/camcorder_color_denoise.py
+++
@@ -1,8 +0,0 @@
-import vapoursynth as vs
-import ccd 
-
-core = vs.core
-
-derainbow = ccd.ccd(video_in, 10, "709")
-
-derainbow.set_output()
--- vs/neo_f3kdb-deband.py
+++
@@ -1,4 +0,0 @@
-import vapoursynth as vs
-core = vs.core
-
-core.neo_f3kdb.Deband(video_in, range=30, y=64, cb=32, cr=32, grainy=64, grainc=0, dynamic_grain=True, sample_mode=2).set_output()
